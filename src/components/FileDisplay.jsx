a6b35d09af6f240e5f089f97ecd9c995af691436
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2024-07-27 18:19:04 +1000

    now we can record audio

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FileDisplay = (props) => {
   const { file, recording, onReset } = props;
-  console.log(typeof file);
+  console.log(recording);
   return (
     <main className="flex-1 flex flex-col p-4 justify-center pb-20  text-center w-fit w-max-full mx-auto">
       <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold p-4">
@@ -11,7 +11,9 @@ const FileDisplay = (props) => {
       </h1>
       <div className="text-left  flex flex-col my-4">
         <p className="font-semibold">Name</p>
-        <p className="font-light text-slate-600">{file.name}</p>
+        <p className="font-light text-slate-600">
+          {file ? file.name : "custom audio "}
+        </p>
       </div>
       <div className="flex justify-between items-center gap-20 pt-4">
         <button
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,8 +7,80 @@ const HomePage = (props) => {
   // a state for the state of our recording ( inactive or recording)
   const [recStat, setRecStat] = useState("inactive");
   // a state for the audio itseld
-  const [audio, setAudio] = useState(null);
-  // a state for
+  const [audio, setAudio] = useState([]);
+  // a state for duration
+  //const [duration, setDuration] = useState(0);
+
+  //
+  const mediaRecorder = useRef(null);
+
+  // it's to know we ain't recording video
+  const inType = "audio/webm";
+
+  async function startRecording() {
+    let currStream;
+
+    // get users perm for the audio rec
+    try {
+      const streamData = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+      currStream = streamData;
+    } catch (e) {
+      console.log(e.message);
+      return;
+    }
+
+    setRecStat("recording");
+
+    // now we want to make a media recording instance
+    const media = new MediaRecorder(currStream, { type: inType });
+    // you send the ref of rcorder
+    mediaRecorder.current = media;
+    // start it
+    mediaRecorder.current.start();
+    let audioChunk = [];
+    // check the data if ok save it
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") {
+        return;
+      }
+      if (event.data.size === 0) {
+        return;
+      }
+      audioChunk.push(event.data);
+    };
+
+    // put the audio in it's place
+    setAudio(audioChunk);
+  }
+
+  async function stopRecording() {
+    // change of state
+    setRecStat("inactive");
+
+    // stop the recorder
+    mediaRecorder.current.stop();
+    // if started make an audio blob , clear audio , set duration to zero and send over the recording
+    mediaRecorder.current.onstop = () => {
+      const audiofile = new Blob(audio, { type: inType });
+      setRecording(audiofile);
+      setAudio([]);
+      //setDuration(0);
+    };
+  }
+
+  // useEffect(() => {
+  //   if (recStat === "inactive") {
+  //     return;
+  //   }
+  //   const interval = setInterval(() => {
+  //     setDuration((CURR) => CURR + 1);
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // });
 
   return (
     <main className="flex-1 flex flex-col p-4 justify-center items-center pb-20">
@@ -20,9 +92,17 @@ const HomePage = (props) => {
         Record <span className="text-purple-400">&rarr;</span> Transcribe{" "}
         <span className="text-purple-400">&rarr;</span> Translate
       </h3>
-      <button className="flex items-center justify-between text-base gap-4 mx-auto w-72 max-w-full my-4 customBtn duration-200 px-4 py-2 rounded-xl">
-        <p>Record</p>
-        <i className="fa-solid fa-microphone"></i>
+      <button
+        className="flex items-center justify-between text-base gap-4 mx-auto w-72 max-w-full my-4 customBtn duration-200 px-4 py-2 rounded-xl"
+        onClick={recStat === "recording" ? stopRecording : startRecording}
+      >
+        <p>{recStat === "inactive" ? "Record" : "Stop Recording"}</p>
+        <i
+          className={
+            "fa-solid fa-microphone " +
+            (recStat === "recording" ? "text-red-500" : "")
+          }
+        ></i>
       </button>
 
       <p>
